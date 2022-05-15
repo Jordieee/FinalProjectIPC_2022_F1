625bba9c2b06840a058a1c096aac594970e9cc84
jordiett <jlincar@epsa.upv.es>
2022-05-15 18:48:49 +0200

    Finished New Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,11 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:final_project_ipc/pages/list_page.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -31,75 +34,113 @@ class LoginPage extends StatelessWidget {
     );
   }
 
-  SizedBox _Header() {
-    return SizedBox(
-        width: double.infinity,
-        height: 125,
-        child: Row(mainAxisAlignment: MainAxisAlignment.start, children: const [
-          Spacer(
-            flex: 1,
-          ),
-          Text("SIGN IN",
+  Widget _Header() {
+    return Column(children: [
+      Container(
+        padding: const EdgeInsets.all(10.0),
+        margin: const EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 0.0),
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+
+          const Text("SIGN IN",
               style: TextStyle(
                 fontSize: 30,
+                fontFamily: 'F1',
                 fontWeight: FontWeight.bold,
               )),
-          Spacer(
-            flex: 5,
+
+          SvgPicture.asset(
+            'assets/img/f1.svg',
           ),
-        ]));
+
+        ]),
+      ),
+      const Divider(
+          color: Colors.grey, thickness: 1, indent: 40, endIndent: 40),
+
+    ]);
   }
 }
 
-Container _Body() {
-  return Container(
+SizedBox _Body() {
+  return SizedBox(
       width: 325,
       height: 230,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.8),
-            spreadRadius: 10,
-            blurRadius: 20,
-          ),
-        ],
-      ),
+
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const Spacer(flex: 5),
+          const Text("Username",
+              style: TextStyle(
+                fontSize: 15,
+                fontFamily: 'F1',
+              )),
+          const Spacer(flex: 1),
+
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
+            children: [
               SizedBox(
-                width: 300,
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.person, color: Colors.indigo),
-                    labelText: 'Username',
-                  ),
-                ),
-              )
+                  width: 300,
+                  child: TextField(
+                    obscureText: false,
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      labelText: "Enter your username",
+                      labelStyle: const TextStyle(
+                          color: Colors.grey
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            width: 2, color: Colors.black),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            width: 2, color: Colors.red),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                  ))
             ],
           ),
-          const Spacer(flex: 1),
+          const Spacer(flex: 3),
+          const Text("Password",
+              style: TextStyle(
+                fontSize: 15,
+                fontFamily: 'F1',
+              )),
+          const Spacer(flex: 2),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
+            children: [
               SizedBox(
-                width: 300,
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    prefixIcon:
-                        Icon(Icons.keyboard_outlined, color: Colors.indigo),
-                    labelText: 'Password',
-                  ),
-                ),
-              )
+                  width: 300,
+                  child: TextField(
+                    obscureText: true,
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      labelText: "Enter your password",
+                      labelStyle: const TextStyle(
+                          color: Colors.grey
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            width: 2, color: Colors.black),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            width: 2, color: Colors.red),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                  ))
             ],
           ),
           const Spacer(flex: 4),
@@ -107,34 +148,79 @@ Container _Body() {
       ));
 }
 
-SizedBox _Footer(BuildContext context) {
-  return SizedBox(
-      width: double.infinity,
-      height: 100,
-      child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-        GestureDetector(
-          child: Container(
-              width: 150,
-              height: 45,
-              decoration: const BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-              ),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Text('SIGN IN',
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.white,
-                        ))
-                  ])),
-          onTap: () {
-            var route = MaterialPageRoute(
-              builder: (context) => ListPage(),
-            );
-            Navigator.of(context).push(route);
-          },
-        )
-      ]));
+Widget _Footer(BuildContext context) {
+  return Button();
 }
+
+class Button extends StatefulWidget {
+  const Button({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<Button> createState() => _ButtonState();
+}
+
+class _ButtonState extends State<Button> {
+  bool pressed = false;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () {
+          setState(() {
+            pressed = true;
+          });
+          var route = MaterialPageRoute(
+            builder: (context) => ListPage(),
+          );
+          Navigator.of(context).push(route);
+        },
+
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(pressed ? Colors.white : Colors.red),
+        ),
+        child: Text(
+          "SIGN IN",
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontFamily: 'F1',
+              color: pressed ? Colors.red : Colors.white,
+              fontSize: 15),
+        ));
+  }
+}
+
+
+/*
+return SizedBox(
+width: double.infinity,
+height: 100,
+child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+GestureDetector(
+child: Container(
+width: 150,
+height: 45,
+decoration: const BoxDecoration(
+color: Colors.red,
+borderRadius: BorderRadius.all(Radius.circular(10)),
+),
+child: Row(
+mainAxisAlignment: MainAxisAlignment.center,
+children: const [
+Text('SIGN IN',
+style: TextStyle(
+fontSize: 20,
+fontFamily: 'F1',
+color: Colors.white,
+))
+])),
+onTap: () {
+
+var route = MaterialPageRoute(
+builder: (context) => ListPage(),
+);
+Navigator.of(context).push(route);
+},
+)
+]));
+*/
